11f617b451f219a66a873c146e86471ef1fb28c8
Lars Michaelis <mail@larmic.de>
2020-01-06 12:20:01 +0100

    add linux and osx binaries

--- install.sh
+++ install.sh
@@ -9,11 +9,14 @@ declare -r RASPI_SCP_FOLDER="pi@raspberrypi.local:/home/pi/test"
 # -a              -> force rebuilding of packages that are already up-to-date.
 # -o gpio-test    -> force to build an executable app file (instead of default https://golang.org/cmd/go/#hdr-Compile_packages_and_dependencies)
 
-echo "Building arm binary '$BINARY_NAME-arm'..."
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -a -o $BINARY_NAME-arm
+echo "Building raspberry pi binary '$BINARY_NAME-pi'..."
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=5 go build -a -o $BINARY_NAME-pi
 
-echo "Building amd64 binary '$BINARY_NAME-amd64'..."
-env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o $BINARY_NAME-amd64
+echo "Building osx binary '$BINARY_NAME-osx'..."
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o $BINARY_NAME-osx
 
-echo "Copy arm binary '$BINARY_NAME-arm' to $RASPI_SCP_FOLDER"
-scp $BINARY_NAME-arm $RASPI_SCP_FOLDER
+echo "Building linux binary '$BINARY_NAME-linux'..."
+env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -a -o $BINARY_NAME-linux
+
+echo "Copy arm binary '$BINARY_NAME-pi' to $RASPI_SCP_FOLDER"
+scp $BINARY_NAME-pi $RASPI_SCP_FOLDER
